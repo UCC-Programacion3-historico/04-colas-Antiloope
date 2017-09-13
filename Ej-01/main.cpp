4bf4283e20290b67286a4a2771399d02809c6d31
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-13 11:39:59 -0300

    Se realizo el ejercicio 01

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include "../Cola/Cola.h"
 
+using namespace std;
+
+bool comparar(Cola<char> *A,Cola<char> *B);
+
 int main() {
-    std::cout << "Ejercicio 04/01\n" << std::endl;
+    Cola<char> A,B;
+    int c, salir=0;
+    char t;
+    do {
+        do {
+            cout << "Desea ingresar un valor a la lista o salir?" << '\n';
+            cout << "  1)Ingresar en A\n  2)Ingresar en B\n  3)Salir" << '\n';
+            cin >> c;
+        } while(c>3 || c<1);
+        switch (c) {
+          case 1:
+            cout << "Ingrese el caracter: ";
+            cin >> t;
+            A.encolar(t);
+            break;
+          case 2:
+            cout << "Ingrese el caracter: ";
+            cin >> t;
+            B.encolar(t);
+            break;
+          case 3:
+            salir = 1;
+        }
+    } while(salir == 0);
+
+    salir = comparar(&A,&B);
+    cout << salir << '\n';
+    return 0;
+}
+bool comparar(Cola<char> *A,Cola<char> *B) {
+    if (A->esVacia() && B->esVacia()) {
+        return 1;
+    }
+    if ((!A->esVacia() && B->esVacia()) || (A->esVacia() && !B->esVacia())) {
+        return 0;
+    }
+    if (A->desencolar() == B->desencolar()) {
+        return comparar(A,B);
+    }
     return 0;
 }
